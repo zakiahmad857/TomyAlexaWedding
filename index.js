ad6132af28d478968cfb88cd93fad41237ff2ed5
syifaandini <syifandin@gmail.com>
2020-07-20 16:49:44 +0700

    early bird modal

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ var parallax = new Parallax(scene);
 // LANGUAGE
 var arrLang = {
   'eng': {
+    // welcoming modal
     'welcome': 'Welcome',
     'language': 'Choose Language',
+    // welcoming modal 2
+    'welcome-desc': 'Video Call Access',
+    'welcome-desc-2': 'We will give you the video call access in',
+    'welcome-desc-3': 'The video call access will show up automatically when the countdown is done.',
+    'welcome-desc-4': 'While waiting, let us show you around the "Reception Venue"',
+    // angpao
     'gift': 'Send Gift with Ease :)',
     'bank': 'Via Bank Transfer',
     'norek': 'Account number'
   },
 
   'ind': {
+    // welcoming modal
     'welcome': 'Selamat Datang',
     'language': 'Pilih Bahasa',
+    // welcoming modal 2
+    'welcome-desc': 'Akses Video Call',
+    'welcome-desc-2': 'Akses video call akan kami berikan dalam waktu',
+    'welcome-desc-3': 'Akses video call akan muncul otomatis ketika hitungan mundur selesai',
+    'welcome-desc-4': 'Sambil menunggu, ayo kita berkeliling venue!',
+    // angpao
     'gift': 'Kirim hadiah dengan mudah :) wkwk',
     'bank': 'Via Transfer Bank',
     'norek': 'No rekening'
@@ -22,7 +36,7 @@ var arrLang = {
 };
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
   // LANGUAGE
   $(document).on('click', 'input[type="checkbox"]', function() {
@@ -32,7 +46,7 @@ $(document).ready(function(){
   var checkboxValues = JSON.parse(localStorage.getItem("checkboxValues")) || {},
     $checkboxes = $("#checkbox-container :checkbox");
 
-    // console.log($checkboxes);
+  // console.log($checkboxes);
   $checkboxes.on("change", function() {
     checkboxValues[this.id] = this.checked;
 
@@ -55,7 +69,7 @@ $(document).ready(function(){
   });
 
 
-  $("#toggleLang").on('change', function(){
+  $("#toggleLang").on('change', function() {
     var tog = $("#toggleLang").prop('checked');
     toggleLanguange(tog);
     console.log(tog);
@@ -63,54 +77,65 @@ $(document).ready(function(){
 
   // MODALS
   // Welcoming and Angpao Modal
-    $("#welcomeModal").css("display", "block");
+  $("#welcomeModal").css("display", "block");
 
   // Angpao Modal, when the user clicks on the angpao-button, open the modal
-    $("#angpao").click(function(){
-      $("#angpaoModal").css("display", "block");
-    });
+  $("#angpao").click(function() {
+    $("#angpaoModal").css("display", "block");
+  });
 
   // When the user clicks on (x), close the modal
-    $("span").click(function(){
-      $("#angpaoModal").css("display", "none");
-    });
+  $("span").click(function() {
+    $("#angpaoModal").css("display", "none");
+  });
 
-    $("#checkbox-ind").click(function(){
-      $("#welcomeModal").delay(1000).queue(function(next){
-        $("#welcomeModal").css("display", "none")
-        tour.init();
-      });
+  $("#checkbox-ind").click(function() {
+    $("#welcomeModal").delay(1000).queue(function(next) {
+      $("#welcomeModal").css("display", "none");
+      $("#earlyBird").css("display", "block");
     });
+  });
 
-    $("#checkbox-eng").click(function(){
-      $("#welcomeModal").delay(1000).queue(function(next){
-        $("#welcomeModal").css("display", "none")
-        tour.init();
-      });
+  $("#checkbox-eng").click(function() {
+    $("#welcomeModal").delay(1000).queue(function(next) {
+      $("#welcomeModal").css("display", "none");
+      $("#earlyBird").css("display", "block");
     });
+  });
 
   // When the user clicks anywhere outside of the modal, close it
-    $(window).click(function(e){
-      if ((e.target.id == "angpaoModal")||(e.target.id == "welcomeModal")) {
-        $("#angpaoModal").css("display", "none");
-        $("#welcomeModal").css("display", "none");
-      }
-    });
+  $(window).click(function(e) {
+    if ((e.target.id == "angpaoModal") || (e.target.id == "welcomeModal") || (e.target.id == "earlyBird")) {
+      $("#angpaoModal").css("display", "none");
+      $("#earlyBird").css("display", "none");
+    }
+  });
 
-    $(document).keypress(function(e){
-      console.log(e);
-      if (e.key == "Escape") {
-        $("#angpaoModal").css("display", "none");
-      }
-    });
+  $("body").keydown(function(e) {
+    if (e.key == "Escape") {
+      $("#angpaoModal").css("display", "none");
+    }
+  });
+
+  $("body").keydown(function(e) {
+    if (e.key == "Escape") {
+      $("#earlyBird").css("display", "none");
+      tour.init();
+    }
+  });
+
+  $("#btn-next").click(function(){
+    $("#earlyBird").css("display", "none");
+    tour.init();
+  });
 
   // ANGPAO
   // Angpao Hover
   $("#angpao").hover(
-    function(){
+    function() {
       $("#angpao").attr("src", "images/open-envelope.svg");
-  },
-    function(){
+    },
+    function() {
       $("#angpao").attr("src", "images/envelope.svg");
     }
   );
@@ -118,41 +143,40 @@ $(document).ready(function(){
 
   // WEB TOUR
   var tour = new Tour({
-    steps: [
-      {
-          element: "#idBulan",
-          title: "Bulan",
-          content: "Here is the ‘pelaminan’ live streaming area. You can see Who’s in the pelaminan, also the next guest names.",
-          placement: "right",
+    steps: [{
+        element: "#idBulan",
+        title: "Bulan",
+        content: "Here is the ‘pelaminan’ live streaming area. You can see Who’s in the pelaminan, also the next guest names.",
+        placement: "right",
       },
       {
-          element: "#idAstronaut",
-          title: "Astronaut",
-          content: "lalalallalalalalallalalalalalalalallalalalalla",
-          placement: "top",
+        element: "#idAstronaut",
+        title: "Astronaut",
+        content: "lalalallalalalalallalalalalalalalallalalalalla",
+        placement: "top",
       },
       {
-          element: "#idPlanet",
-          title: "Planet",
-          content: "lalalallalalalalallalalalalalalalallalalalalla",
+        element: "#idPlanet",
+        title: "Planet",
+        content: "Here is the ‘pelaminan’ live streaming area. You can see Who’s in the pelaminan, also the next guest names.",
       },
       {
-          element: "#idRocket",
-          title: "Rocket",
-          content: "lalalallalalalalallalalalalalalalallalalalalla",
-          placement: "left",
+        element: "#idRocket",
+        title: "Rocket",
+        content: "lalalallalalalalallalalalalalalalallalalalalla",
+        placement: "left",
       },
       {
-          element: "#angpao",
-          title: "Angpao gift for the newly wed",
-          content: "Don’t worry you can still send your gift, see more info here ;)",
-          placement: "right",
+        element: "#angpao",
+        title: "Angpao gift for the newly wed",
+        content: "Don’t worry you can still send your gift, see more info here ;)",
+        placement: "right",
       },
       {
-          element: "#info",
-          title: "Instructions",
-          content: "If you want to re-read these instructions again just click this icon",
-          placement: "bottom",
+        element: "#info",
+        title: "Instructions",
+        content: "If you want to re-read these instructions again just click this icon",
+        placement: "bottom",
       }
     ],
     container: 'body',
@@ -163,19 +187,19 @@ $(document).ready(function(){
     debug: false,
     backdropPadding: 0,
     redirect: true,
-    template:'<div class="popover tour" role="tooltip"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <p class="steps">Step 1 of 7</p> <div class="btn-group"> <button class="btn btn-sm btn-secondary btn-prev" data-role="prev">&lt;</button> <button class="btn btn-sm btn-secondary btn-next" data-role="next">Next &gt;</button> <button class="btn btn-sm btn-secondary" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-secondary" data-role="end">End tour</button> </div> </div>',
+    template: '<div class="popover tour" role="tooltip"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <p class="steps">Step 1 of 7</p> <div class="btn-group"> <button class="btn btn-sm btn-secondary btn-prev" data-role="prev">&lt;</button> <button class="btn btn-sm btn-secondary btn-next" data-role="next">Next &gt;</button> <button class="btn btn-sm btn-secondary" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-secondary" data-role="end">End tour</button> </div> </div>',
   });
 
 
-  $("#info").click(function(){
+  $("#info").click(function() {
     tour.restart();
   });
 
 });
 
 // lANGUANGE FUNCTIONS
-function selectLanguage(check){
-  if(check.id === "checkbox-ind" && check.checked){
+function selectLanguage(check) {
+  if (check.id === "checkbox-ind" && check.checked) {
     var lang = "ind";
     $("#toggleLang").prop('checked', false);
     $(".lang").each(function(index, element) {
@@ -183,7 +207,7 @@ function selectLanguage(check){
     });
   }
 
-  if(check.id === "checkbox-eng" && check.checked){
+  if (check.id === "checkbox-eng" && check.checked) {
     var lang = "eng";
     $("#toggleLang").prop('checked', true);
     $(".lang").each(function(index, element) {
@@ -192,14 +216,14 @@ function selectLanguage(check){
   }
 }
 
-function toggleLanguange(tog){
+function toggleLanguange(tog) {
   console.log("func: " + tog);
-  if(tog){
+  if (tog) {
     var lang = "eng";
     $(".lang").each(function(index, element) {
       $(this).text(arrLang[lang][$(this).attr('key')]);
     });
-  } else{
+  } else {
     var lang = "ind";
     $(".lang").each(function(index, element) {
       $(this).text(arrLang[lang][$(this).attr('key')]);
