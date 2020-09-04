11463787ce49bbc25806c94668b857b0c6c94f3d
syifaandini <syifandin@gmail.com>
2020-09-05 05:52:43 +0700

    button on off tv 768

--- index.js
+++ index.js
@@ -196,6 +196,23 @@ var arrLang = {
   }
 };
 
+// Disable Zoom
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+    // 107 Num Key  +
+    // 109 Num Key  -
+    // 173 Min Key  hyphen/underscor Hey
+    // 61 Plus key  +/= key
+});
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+
 /* Detect-zoom
  * -----------
  * Cross Browser Zoom and Pixel Ratio Detector
@@ -762,9 +779,19 @@ $(document).ready(function() {
   }
 
   // Off MC
+  $(".on").click(function() {
+    $("#videoMC").css("display", "none");
+    $(".screen-wrapper").css("display", "block");
+    $(".on").css("display", "none");
+    $(".off").css("display", "block")
+    toggleSound();
+  });
+
   $(".off").click(function() {
-    $("#videoMC").toggleClass("offMC");
-    $(".screen-wrapper").toggleClass("screenOff");
+    $("#videoMC").css("display", "block");
+    $(".screen-wrapper").css("display", "none");
+    $(".on").css("display", "block");
+    $(".off").css("display", "none")
     toggleSound();
   });
 
